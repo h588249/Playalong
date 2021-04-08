65756d17f036310a4e56243bce4b979a9db28016
Olav Wessel Pete <olav.pete@gmail.com>
2021-04-08 14:53:02 +0200

    fixed some initial stuff

--- src/main/java/utility/MappingUtility.java
+++ src/main/java/utility/MappingUtility.java
@@ -3,17 +3,17 @@ package utility;
 public class MappingUtility
 {
     public static final String ADMIN_URL = "admin";
-    public static final String ADMIN_PATH = "WEB-INF/admin/temporary_admin.jsp";
+    public static final String ADMIN_PATH = "jsp/admin.jsp";
 
     public static final String INDEX_URL = "index";
-    public static final String INDEX_PATH = "index.jsp";
+    public static final String INDEX_PATH = "jsp/index.jsp";
 
     public static final String LOGIN_URL = "login";
-    public static final String LOGIN_PATH = "WEB-INF/login/login.jsp";
+    public static final String LOGIN_PATH = "jsp/login.jsp";
 
     public static final String LOGOUT_URL = "logout";
-    public static final String LOGOUT_PATH = "WEB-INF/login/logout.jsp";
+    public static final String LOGOUT_PATH = "jsp/logout.jsp";
 
     public static final String REGISTERUSER_URL = "register";
-    public static final String REGISTERUSER_PATH = "WEB-INF/registeruser/registeruser.jsp";
+    public static final String REGISTERUSER_PATH = "jsp/createUser.jsp";
 }
