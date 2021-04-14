477b788b19f6c160019f818ff51893a29ab77489
TheSaltChip <seb2500@gmail.com>
2021-04-14 21:04:58 +0200

    Update SearchServlet.java
    redirects to register instead of index

--- src/main/java/controller/search/SearchServlet.java
+++ src/main/java/controller/search/SearchServlet.java
@@ -1,10 +1,5 @@
 package controller.search;
 
-import model.song.Song;
-import repository.Repository;
-import repository.song.SongDAO;
-
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,20 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static utility.MappingUtility.*;
-import static utility.ServletUtility.initialize;
 import static utility.ServletUtility.validate;
 
-@WebServlet(name = "SearchServlet", value = "/"+ SEARCH_URL)
+@WebServlet(name = "SearchServlet", value = "/" + SEARCH_URL)
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(!validate(req, resp, SEARCH_URL)){
-            resp.sendRedirect(LOGIN_URL);
+        if (!validate(req, resp, SEARCH_URL)) {
+            resp.sendRedirect(REGISTER_USER_URL);
             return;
         }
 
