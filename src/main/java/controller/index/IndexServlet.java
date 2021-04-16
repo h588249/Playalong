bc5e4bb7c4241a957f3717224fad676323b276e1
TheSaltChip <seb2500@gmail.com>
2021-04-16 19:51:06 +0200

    Update IndexServlet.java
    Supplies the index page with all the song names

--- src/main/java/controller/index/IndexServlet.java
+++ src/main/java/controller/index/IndexServlet.java
@@ -1,23 +1,40 @@
 package controller.index;
 
-import static utility.MappingUtility.*;
-import static utility.ServletUtility.*;
+import model.song.Song;
+import org.apache.commons.text.StringEscapeUtils;
+import repository.Repository;
+import repository.song.SongDAO;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static utility.MappingUtility.INDEX_PATH;
+import static utility.ServletUtility.initialize;
+
 @WebServlet(name = "IndexServlet", value = "/index")
-public class IndexServlet extends HttpServlet
-{
+public class IndexServlet extends HttpServlet {
+    @EJB
+    private Repository<Song> repository;
+
+    private SongDAO songDAO = null;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
+        songDAO = (SongDAO) initialize(songDAO, new SongDAO(repository));
+
+        String songList = songDAO.getAllSongs()
+                .stream()
+                .map(Song::getName)
+                .map(StringEscapeUtils::escapeHtml4)
+                .reduce("", (a, b) -> a + "#$" + b);
+        request.setAttribute("songs", songList.substring(2));
+
         request.getRequestDispatcher(INDEX_PATH).forward(request, response);
     }
 }
